e368204e71efed3a1a5f5df7815f8ada95820d56
cogocreative@gmail.com <D4funk#17>
2019-03-13 19:09:35 +0100

    docs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-import React from 'react';
-// export * from './atoms';
-// export * from './utils';
-
-const Button = () => (
-  <button type="button">test</button>
-);
-
-export { Button };
+export * from './atoms';
+export * from './utils';
